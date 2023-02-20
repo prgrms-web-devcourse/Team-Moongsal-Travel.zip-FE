da31b26d16baa1f3436b2f67d1644bfcbc4a419d
kmww <kmw9708@gmail.com>
2023-02-21 00:47:04 +0900

    chore: husky

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,16 +1,10 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `yarn lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
-
-  // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx)': (filenames) => {
-    return [
-      `yarn eslint --fix ${filenames.join(' ')}`,
-      `yarn prettier --write ${filenames.join(' ')}`,
-    ];
-  },
-
-  '**/*.tsx': (filenames) => {
-    return `yarn stylelint --ignore-path .gitignore ${filenames.join(' ')}`;
-  },
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
 };
---
+++ src/pages/Count.tsx
@@ -0,0 +1,5 @@
+const Count = () => {
+  return <div>123</div>;
+};
+
+export default Count;
