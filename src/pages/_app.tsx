b4bba1f1303103bd48b88d04423b822c753f00eb
kmww <kmw9708@gmail.com>
2023-02-22 02:21:45 +0900

    design: 글로벌 폰트 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
 
+import GlobalStyle from '@/styles/GlobalStyle';
 import MobileLayout from '@/styles/MobileLayout';
 import { theme } from '@/styles/MuiTheme';
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <ThemeProvider theme={theme}>
         <MobileLayout>
+          <GlobalStyle />
           <Component {...pageProps} />
         </MobileLayout>
       </ThemeProvider>
--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,9 +1,29 @@
 import { css, Global } from '@emotion/react';
 
 const GlobalStyle = () => {
-  return <Global styles={style}></Global>;
+  return <Global styles={style} />;
 };
 
 export default GlobalStyle;
 
-const style = css``;
+const style = css`
+  @font-face {
+    font-weight: normal;
+    font-family: 'KOHINanumOTFL';
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/KOHINanumOTFL.woff')
+      format('woff');
+  }
+
+  @font-face {
+    font-weight: normal;
+    font-family: 'KOHIBaeumOTF';
+    font-style: normal;
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/KOHIBaeumOTF.woff')
+      format('woff');
+  }
+
+  body {
+    font-family: 'KOHINanumOTFL', serif;
+  }
+`;
