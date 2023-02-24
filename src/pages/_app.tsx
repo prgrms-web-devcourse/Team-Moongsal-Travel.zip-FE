26f8ddb8d15701b786d33b21406c27782f7c170e
cojet <kmw9708@gmail.com>
2023-02-24 14:50:39 +0900

    refactor: 모든페이지 헤더 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material';
 import type { AppProps } from 'next/app';
 
+import { Header } from '@/components/Header';
 import GlobalStyle from '@/styles/GlobalStyle';
 import MobileLayout from '@/styles/MobileLayout';
 import { theme } from '@/styles/MuiTheme';
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider theme={theme}>
         <MobileLayout>
           <GlobalStyle />
+          <Header />
           <Component {...pageProps} />
         </MobileLayout>
       </ThemeProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 
-import { Header } from '@/components/Header';
-
 export default function Home() {
   return (
     <>
@@ -12,7 +10,6 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <Header />
         <div>Home</div>
       </main>
     </>
