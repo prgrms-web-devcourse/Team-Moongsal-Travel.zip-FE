b28347d77c9082ea83bfa8243999ba48f930c9e4
kmww <kmw9708@gmail.com>
2023-02-27 01:15:07 +0900

    refactor: PostInfo 컴포넌트 분리

--- src/components/PostDetail/PostInfo.tsx
+++ src/components/PostDetail/PostInfo.tsx
@@ -1,17 +1,8 @@
-import {
-  DateRangeOutlined,
-  DirectionsBus,
-  DirectionsCar,
-  Flight,
-  Payment,
-  Public,
-  VisibilityOutlined,
-} from '@mui/icons-material';
-import { Avatar, Box, Grid } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { Stack } from '@mui/system';
+import { Box } from '@mui/material';
 
-import { SubTitle, Title } from '@/components/common';
+import { Title } from '@/components/common';
+
+import { PostCategory, PostProfile } from './';
 
 interface PostInfo {
   authority: string;
@@ -23,18 +14,7 @@ const PostInfo = ({ authority }: PostInfo) => {
       <Title bold='bold'>여기 제목</Title>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: '1rem' }}>
         <Box sx={{ display: 'flex', gap: '1rem' }}>
-          <Box sx={{ display: 'flex', alignItems: 'flex-start' }}>
-            <Avatar src='default' sx={{ width: '1rem', height: '1rem' }} />
-            <SubTitle fontSize='0.8rem' color='gray030.main'>
-              닉네임
-            </SubTitle>
-          </Box>
-          <Box sx={{ display: 'flex', alignItems: 'flex-start' }}>
-            <VisibilityOutlined color='gray030' sx={{ width: '1rem', height: '1rem' }} />
-            <SubTitle fontSize='0.8rem' color='gray030.main'>
-              123123회
-            </SubTitle>
-          </Box>
+          <PostProfile />
         </Box>
         {authority === 'writer' && (
           <Box sx={{ display: 'flex', gap: '1rem' }}>
@@ -44,57 +24,10 @@ const PostInfo = ({ authority }: PostInfo) => {
         )}
       </Box>
       <Box sx={{ width: '80%' }}>
-        <Grid container rowSpacing={1}>
-          <Grid item xs={6}>
-            <Underline>여행지</Underline>
-            <Stack flexDirection='row' mt='0.3rem'>
-              <Public color='gray030' />
-              <SubTitle color='gray030.main' bold='bold' fontSize='1rem'>
-                일본
-              </SubTitle>
-            </Stack>
-          </Grid>
-          <Grid item xs={6}>
-            <Underline>여행기간</Underline>
-            <Stack flexDirection='row' mt='0.3rem'>
-              <DateRangeOutlined color='gray030' />
-              <SubTitle color='gray030.main' bold='bold' fontSize='1rem'>
-                3박4일
-              </SubTitle>
-            </Stack>
-          </Grid>
-          <Grid item xs={6}>
-            <Underline>여행경비</Underline>
-            <Stack flexDirection='row' mt='0.3rem'>
-              <Payment color='gray030' />
-              <SubTitle color='gray030.main' bold='bold' fontSize='1rem'>
-                138만원
-              </SubTitle>
-            </Stack>
-          </Grid>
-          <Grid item xs={6}>
-            <Underline>이동수단</Underline>
-            <Stack flexDirection='row' mt='0.3rem'>
-              {/* SvgIcon 조건별 출력
-            <SvgIcon component={아이콘 이름} /> */}
-              <Flight color='gray030' />
-              <DirectionsBus color='gray030' />
-              <DirectionsCar color='gray030' />
-            </Stack>
-          </Grid>
-        </Grid>
+        <PostCategory />
       </Box>
     </Box>
   );
 };
 
 export default PostInfo;
-
-const Underline = styled('div')(({ theme }) => ({
-  '&:after': {
-    display: 'block',
-    width: '8rem',
-    borderBottom: `solid 1px ${theme.palette.gray020.main}`,
-    content: '""',
-  },
-}));
