7b7f32aa3546c5aa3563befeee2ac71acbdd899e
hyejun <solar3070@naver.com>
2023-02-26 19:15:50 +0900

    feat: 인증 코드 요청, 확인 api 코드 작성

--- src/api/hooks/user.ts
+++ src/api/hooks/user.ts
@@ -1,14 +1,30 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { postUserRegister } from '@/api/user';
+import { postSendEmail, postUserRegister, postVerifyCode } from '@/api/user';
 import { UserRegister } from '@/api/user/type';
 
 export const usePostUserRegister = () => {
   return useMutation({
-    mutationFn: async (data: UserRegister) => {
-      const response = await postUserRegister(data);
-      return response;
+    mutationFn: async (data: UserRegister) => await postUserRegister(data),
+    onError: (error: { message: string }) => {
+      console.error(error.message);
     },
+  });
+};
+
+export const usePostSendEmail = () => {
+  return useMutation({
+    mutationFn: async (data: { email: string }) => await postSendEmail(data),
+    onError: (error: { message: string }) => {
+      console.error(error.message);
+    },
+  });
+};
+
+export const usePostVerifyCode = () => {
+  return useMutation({
+    mutationFn: async (data: { email: string; code: string }) =>
+      await postVerifyCode(data),
     onError: (error: { message: string }) => {
       console.error(error.message);
     },
