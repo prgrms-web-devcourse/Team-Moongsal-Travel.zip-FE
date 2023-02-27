ad675e9b2d6b7125c27d4a6a0bf5393f44d69bb1
kmww <kmw9708@gmail.com>
2023-02-27 16:51:05 +0900

    refactor: libaries 타입설정

--- src/components/SearchModal/AutoComplete.tsx
+++ src/components/SearchModal/AutoComplete.tsx
@@ -2,10 +2,18 @@ import { useLoadScript } from '@react-google-maps/api';
 
 import { PlacesAutocomplete } from './';
 
+const libraries: (
+  | 'drawing'
+  | 'geometry'
+  | 'localContext'
+  | 'places'
+  | 'visualization'
+)[] = ['places'];
+
 const AutoComplete = () => {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_KEY as string,
-    libraries: ['places'],
+    libraries,
   });
 
   if (!isLoaded) return <div>Loading...</div>;
