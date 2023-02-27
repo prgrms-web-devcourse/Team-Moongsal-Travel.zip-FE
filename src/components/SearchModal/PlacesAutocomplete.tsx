b7e77b32f6988a2731efaa7c6a12e850b2dce5e1
kmww <kmw9708@gmail.com>
2023-02-27 18:13:20 +0900

    design: 자동완성 디자인

--- src/components/SearchModal/PlacesAutocomplete.tsx
+++ src/components/SearchModal/PlacesAutocomplete.tsx
@@ -1,11 +1,10 @@
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxList,
-  ComboboxOption,
-  ComboboxPopover,
-} from '@reach/combobox';
+import { LocationOn } from '@mui/icons-material';
+import { Box, Grid, Stack, Typography } from '@mui/material';
+import { Combobox, ComboboxInput, ComboboxList, ComboboxPopover } from '@reach/combobox';
+import { Fragment } from 'react';
 import usePlacesAutocomplete from 'use-places-autocomplete';
+
+const PLACEHOLDER_SEARCH = '도시 또는 키워드를 입력해주세요';
 
 const PlacesAutocomplete = () => {
   const {
@@ -24,16 +23,31 @@ const PlacesAutocomplete = () => {
   return (
     <Combobox onSelect={handleSelect}>
       <ComboboxInput
+        placeholder={PLACEHOLDER_SEARCH}
         value={value}
         onChange={(e) => setValue(e.target.value)}
         disabled={!ready}
       />
       <ComboboxPopover>
         <ComboboxList>
-          {status === 'OK' &&
-            data.map(({ place_id, description }) => (
-              <ComboboxOption key={place_id} value={description} />
-            ))}
+          <Grid container alignItems='center'>
+            <Grid item sx={{ width: 'calc(100% -44px)', wordWrap: 'break-word' }}>
+              {status === 'OK' &&
+                data.map(({ place_id, structured_formatting }) => (
+                  <Fragment key={place_id}>
+                    <Grid item sx={{ display: 'flex', width: 44 }}>
+                      <LocationOn />
+                    </Grid>
+                    <Stack>
+                      <Box component='span'>{structured_formatting.main_text}</Box>
+                      <Typography variant='body2' color='primary'>
+                        {structured_formatting.secondary_text}
+                      </Typography>
+                    </Stack>
+                  </Fragment>
+                ))}
+            </Grid>
+          </Grid>
         </ComboboxList>
       </ComboboxPopover>
     </Combobox>
@@ -41,3 +55,23 @@ const PlacesAutocomplete = () => {
 };
 
 export default PlacesAutocomplete;
+
+// const SearchInputStyle = {
+//   width: '100%',
+//   '&.MuiInput-root': {
+//     color: 'white.main',
+//     fontWeight: 'bold',
+//     padding: 0,
+//     '&:hover': {
+//       borderBottomColor: 'white.main',
+//     },
+//   },
+//   '&.MuiInput-root:before': {
+//     borderBottom: '2px solid',
+//     borderBottomColor: 'white.main',
+//   },
+//   '&.MuiInput-root:after': {
+//     borderBottom: '2px solid',
+//     borderBottomColor: 'gray030.main',
+//   },
+// };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,10 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>
-        <div>Home</div>
-        <AutoComplete />
+        <div>
+          <div>Home</div>
+          <AutoComplete />
+        </div>
       </main>
     </>
   );
