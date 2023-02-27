48af136d3dc5e366bda771d540117b4f5dd79514
parkmoen <pmj6516@naver.com>
2023-02-27 21:33:25 +0900

    feat: 공통 비제어 입력 컴포넌트 구현

--- src/components/Login/Local.tsx
+++ src/components/Login/Local.tsx
@@ -1,17 +1,65 @@
-import { Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Stack } from '@mui/material';
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
+import CommonInput from '../common/CommonInput';
+
+interface FormState {
+  email: string;
+  password: string;
+}
 
 const Local = () => {
+  const { control, handleSubmit } = useForm<FormState>({
+    mode: 'onSubmit',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: FormState) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <Stack spacing={4}>
-        <TextField id='outlined-basic' label='이메일' variant='outlined' />
-        <TextField id='outlined-basic' label='비밀번호' variant='outlined' />
+      <Stack spacing={2} component='form' onSubmit={handleSubmit(onSubmit)}>
+        <CommonInput<FormState>
+          id='email'
+          label='이메일'
+          variant='outlined'
+          control={control}
+          name='email'
+          rules={{
+            required: '이메일은 필수 입력입니다.',
+            pattern: {
+              value: /[A-Za-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+              message: '이메일 형식에 맞지 않습니다.',
+            },
+          }}
+        />
+        <CommonInput<FormState>
+          id='password'
+          label='비밀번호'
+          variant='outlined'
+          control={control}
+          name='password'
+          rules={{
+            required: '비밀번호는 필수 입력입니다.',
+            pattern: {
+              value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~!@#$%^&*])[A-Za-z\d~!@#$%^&*]{8,}$/,
+              message: '비밀번호 형식에 맞지 않습니다.',
+            },
+          }}
+        />
+        <Box width='100%'>
+          <Button type='submit' variant='contained' sx={buttonStyle}>
+            로그인
+          </Button>
+        </Box>
       </Stack>
-      <Button variant='contained' sx={buttonStyle}>
-        로그인
-      </Button>
 
       <Stack spacing={6} direction='row' justifyContent='center'>
         <Link href='/'>비밀번호 찾기</Link>
@@ -24,8 +72,9 @@ const Local = () => {
 export default Local;
 
 const buttonStyle = {
-  marginY: 5.5,
+  marginY: 4,
   fontSize: '1rem',
   paddingY: 0,
   height: 56,
+  width: '100%',
 } as const;
---
+++ src/components/common/CommonInput.tsx
@@ -0,0 +1,30 @@
+import { TextField, TextFieldProps } from '@mui/material';
+import React from 'react';
+import { FieldValues, useController } from 'react-hook-form';
+
+import { FormControlType } from '@/types/common';
+
+type InputProps<T extends FieldValues> = FormControlType<T> & TextFieldProps;
+
+const CommonInput = <T extends FieldValues>({
+  name,
+  control,
+  rules,
+  ...props
+}: InputProps<T>) => {
+  const {
+    field: { value, onChange },
+    fieldState: { error },
+  } = useController({ name, control, rules });
+
+  return (
+    <TextField
+      value={value}
+      onChange={onChange}
+      helperText={error && error.message}
+      {...props}
+    />
+  );
+};
+
+export default CommonInput;
