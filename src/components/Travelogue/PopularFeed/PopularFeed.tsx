eecc5e39ed22a92e546cfdd09ef848cb81c1fafd
parkmoen <pmj6516@naver.com>
2023-03-05 18:16:54 +0900

    feat: 게시글 목록 캐러셀 기능 구현하기

---
+++ src/components/Travelogue/PopularFeed/PopularFeed.tsx
@@ -0,0 +1,34 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import Slider from 'react-slick';
+
+import { useGetRecentTravelogue } from '@/api/hooks/travelogue';
+
+import TravelogueFeed from '../TravelogueFeed';
+
+const PopularFeed = () => {
+  const { data: travelogues } = useGetRecentTravelogue();
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  if (!travelogues || travelogues.length === 0) {
+    return null;
+  }
+
+  return (
+    <Slider {...settings}>
+      {travelogues.map((travelogue) => {
+        return <TravelogueFeed key={String(travelogue.travelogueId)} data={travelogue} />;
+      })}
+    </Slider>
+  );
+};
+
+export default PopularFeed;
