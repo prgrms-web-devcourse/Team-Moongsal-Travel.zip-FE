772069d49ad355ca8335e12640dfd76f7b022e27
parkmoen <pmj6516@naver.com>
2023-03-07 12:01:42 +0900

    design: 프로필 아바타 UI 컴포넌트 공통화 디자인

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,7 @@
 export { default as AlertMessage } from './AlertMessage';
 export { default as CommonInput } from './CommonInput';
 export { default as FileInput } from './FileInput';
+export { default as ProfileAvatar } from './ProfileAvatar';
 export { default as Row } from './Row';
 export { default as SubTitle } from './SubTitle';
 export { default as TemporaryButton } from './TemporaryButton';
