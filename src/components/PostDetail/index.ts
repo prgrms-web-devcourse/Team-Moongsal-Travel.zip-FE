666ebd290eb869ce7054d529e2e68df10ee993ad
hyejun <solar3070@naver.com>
2023-03-10 04:46:35 +0900

    feat: 트래블로그 상단 컴포넌트 분리

--- src/components/PostDetail/index.ts
+++ src/components/PostDetail/index.ts
@@ -1,5 +1,5 @@
 export { default as PostContents } from './PostContents';
-export { default as PostInfo } from './PostInfo';
 export { default as TravelInfo } from './TravelInfo';
 export { default as TravelInfoItem } from './TravelInfoItem';
+export { default as TravelogueHeader } from './TravelogueHeader';
 export { default as TravelogueInfo } from './TravelogueInfo';
