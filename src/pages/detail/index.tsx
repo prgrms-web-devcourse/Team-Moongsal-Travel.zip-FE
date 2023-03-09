8f5988278d619ded46cdcb32ae56e764bbb92370
hyejun <solar3070@naver.com>
2023-03-09 16:36:17 +0900

    feat: 트래블로그 상세 조회 데이터 state 저장

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,14 +1,41 @@
+import { Box, Stack } from '@mui/material';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { usePatchTravelogueDetailById } from '@/api/hooks/travelogue';
+import { PostContents, PostInfo } from '@/components/PostDetail';
+import { TravelogueDetailType } from '@/types/travelogue';
 
 const Detail = () => {
-  const {
-    query: { travelogueId },
-  } = useRouter();
+  const router = useRouter();
+  const authority = 'writer'; // viewer
+  const { mutate } = usePatchTravelogueDetailById();
+  const [travelogueDetail, setTravelogueDetail] = useState<TravelogueDetailType>();
 
-  console.log(travelogueId, typeof travelogueId);
+  console.log('travelogueDetail', travelogueDetail);
 
-  return <div>Detail</div>;
+  useEffect(() => {
+    const { travelogueId } = router.query;
+    if (travelogueId) {
+      mutate(
+        { travelogueId: travelogueId as string },
+        {
+          onSuccess: ({ data }) => {
+            setTravelogueDetail(data);
+          },
+        },
+      );
+    }
+  }, [mutate, router.isReady, router.query]);
+
+  return (
+    <Box sx={{ px: '24px' }}>
+      <Stack spacing={5}>
+        <PostInfo authority={authority} />
+        <PostContents />
+      </Stack>
+    </Box>
+  );
 };
 
 export default Detail;
