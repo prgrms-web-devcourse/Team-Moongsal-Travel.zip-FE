7abd3627ba6c58a3840e96810104f61eecb0d8e9
kmww <kmw9708@gmail.com>
2023-03-11 18:57:24 +0900

    폴더 추가 버튼

--- src/pages/scrap/index.tsx
+++ src/pages/scrap/index.tsx
@@ -1,17 +1,23 @@
 import { Delete as DeleteIcon, Folder as FolderIcon } from '@mui/icons-material';
 import {
   Avatar,
+  Box,
   Button,
   IconButton,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Stack,
+  styled,
+  SwipeableDrawer,
+  TextField,
   Typography,
 } from '@mui/material';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { getScrapDocument } from '@/api/scrap';
+import { SubTitle } from '@/components/common';
 
 const DUMMY_DATA = {
   list: [
@@ -23,6 +29,7 @@ const DUMMY_DATA = {
 
 const Scrap = () => {
   // const [scrapDocs, setScrapDocs] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const fetchScrapDoc = async () => {
@@ -32,11 +39,42 @@ const Scrap = () => {
     fetchScrapDoc();
   }, []);
 
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen);
+  };
+
   return (
     <>
-      <Typography sx={{ mt: 4, mb: 2 }} variant='h6' component='div'>
-        스크랩
-      </Typography>
+      <Stack>
+        <Typography sx={{ mt: 4, mb: 2 }} variant='h6' component='div'>
+          나만의 장소
+        </Typography>
+        <Button onClick={toggleDrawer(true)}>폴더 추가</Button>
+        <SwipeableDrawer
+          container={() => document.body}
+          anchor='bottom'
+          open={open}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+          disableScrollLock
+          sx={swipeStyle}
+          ModalProps={{
+            keepMounted: false,
+          }}>
+          <Puller />
+          <Box sx={{ pb: 2 }} />
+          <Box>
+            <Box component='form'>
+              <SubTitle>나만의 장소 추가</SubTitle>
+              <TextField fullWidth placeholder='폴더 이름을 적어주세요' sx={{ mt: 2 }} />
+              <Stack flexDirection='row' justifyContent='flex-end' mt={2}>
+                <Button>취소</Button>
+                <Button>생성</Button>
+              </Stack>
+            </Box>
+          </Box>
+        </SwipeableDrawer>
+      </Stack>
       <List dense={false}>
         {DUMMY_DATA.list.map(({ title, storageObjectId }) => (
           <ListItem
@@ -55,9 +93,31 @@ const Scrap = () => {
           </ListItem>
         ))}
       </List>
-      <Button>폴더 추가</Button>
     </>
   );
 };
 
 export default Scrap;
+
+const Puller = styled(Box)(({ theme }) => ({
+  width: 30,
+  height: 6,
+  backgroundColor: theme.palette.gray030.main,
+  borderRadius: 3,
+  position: 'absolute',
+  top: 8,
+  left: 'calc(50% - 15px)',
+}));
+
+const swipeStyle = {
+  '&.MuiDrawer-root > .MuiPaper-root': {
+    boxSizing: 'border-box',
+    minWidth: '390px',
+    maxWidth: '414px',
+    margin: 'auto',
+    padding: '1rem 2rem',
+    overflow: 'visible',
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+} as const;
