47feb24bf330ee071829cf0798e44fc41cd12323
hyejun <solar3070@naver.com>
2023-03-12 01:35:18 +0900

    fix: 기간 배열 매개변수 타입 number로 변경

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -4,6 +4,6 @@ export const getDateInfo = (date: Dayjs) => {
   return date.format('YYYY-MM-DD');
 };
 
-export const createPeriodArray = (days: string) => {
-  return Array.from({ length: parseInt(days) }, (_, i) => `${i + 1}일차`);
+export const createPeriodArray = (days: number) => {
+  return Array.from({ length: days }, (_, i) => `${i + 1}일차`);
 };
