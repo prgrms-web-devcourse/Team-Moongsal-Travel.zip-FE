a189c7c9a72c8ad83a63e50f9dc6003bbd9bdaa8
hyejun <solar3070@naver.com>
2023-03-12 04:18:43 +0900

    feat: 서브 트래블로그 지역 자동 완성 구현

--- src/components/SubTravelogue/Location.tsx
+++ src/components/SubTravelogue/Location.tsx
@@ -1,7 +1,10 @@
 import { LocationOnOutlined } from '@mui/icons-material';
-import { Box, IconButton, OutlinedInput } from '@mui/material';
+import { Box, IconButton, styled } from '@mui/material';
+import { Combobox, ComboboxInput } from '@reach/combobox';
 import { ControllerRenderProps } from 'react-hook-form';
 
+import { AutoCompleteList } from '@/components/SearchModal';
+import useAutoComplete from '@/hooks/useAutoComplete';
 import { SubTravelogueType } from '@/types/post';
 
 interface LocationProps {
@@ -9,13 +12,32 @@ interface LocationProps {
 }
 
 const Location = ({ field }: LocationProps) => {
+  const { ready, setValue, suggestions, handleSelect } = useAutoComplete();
+
+  const handleRegionSelect = async (region: string) => {
+    field?.onChange(region);
+    handleSelect(region);
+  };
+
   return (
-    <Box sx={locationBoxStyle}>
-      <OutlinedInput {...field} fullWidth placeholder='지역을 입력하세요' type='text' />
-      <IconButton sx={{ position: 'absolute', right: 0, top: '0.5rem' }}>
-        <LocationOnOutlined />
-      </IconButton>
-    </Box>
+    <Combobox onSelect={handleRegionSelect} style={{ width: '70%' }}>
+      <Box sx={locationBoxStyle}>
+        <SearchInput
+          {...field}
+          placeholder='장소를 입력하세요'
+          value={field?.value}
+          onChange={(e) => {
+            field?.onChange(e.target.value);
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+        />
+        <IconButton sx={{ position: 'absolute', right: '2px', top: '0.5rem' }}>
+          <LocationOnOutlined />
+        </IconButton>
+        <AutoCompleteList suggestions={suggestions} />
+      </Box>
+    </Combobox>
   );
 };
 
@@ -27,3 +49,17 @@ const locationBoxStyle = {
   position: 'relative',
   width: '100%',
 };
+
+const SearchInput = styled(ComboboxInput)(() => ({
+  width: '100%',
+  height: '1.4375em',
+  padding: '16.5px 14px',
+  border: '1px solid rgba(0, 0, 0, 0.23)',
+  borderRadius: '4px',
+  font: 'normal 400 1rem/1.4375em KOHINanumOTFL, sans-serif',
+  color: 'rgba(0, 0, 0, 0.87)',
+  '&:focus': {
+    border: `2px solid #1976d2`,
+    outline: 'none',
+  },
+}));
