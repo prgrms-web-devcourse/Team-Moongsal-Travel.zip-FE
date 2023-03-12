516ff92231e3ce779b821e5ddd1bafbbaac33981
hyejun <solar3070@naver.com>
2023-03-12 14:51:17 +0900

    feat: 트래블로그 수정, 최종 발행 api 연동

--- src/pages/post/first.tsx
+++ src/pages/post/first.tsx
@@ -1,14 +1,17 @@
-import { Box, Button } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-import { useGetTravelogueForEdit } from '@/api/hooks/post';
-import { patchTravelogueForEdit, postTravelogue } from '@/api/post';
+import {
+  useGetTravelogueForEdit,
+  usePatchTraveloguePublish,
+  useSaveTravelogue,
+} from '@/api/hooks/post';
 import { PostBasic } from '@/components/CreatePost';
 import { travelogueFormProps } from '@/constants/defaultFormValue';
 import useImageUpload from '@/hooks/useImageUpload';
-import { TravelogueFormType, TravelogueResponseType } from '@/types/post';
+import { TravelogueFormType, TravelogueSaveResponseType } from '@/types/post';
 import { createPeriodArray } from '@/utils/helper';
 import { setItem } from '@/utils/storage';
 
@@ -16,10 +19,11 @@ const First = () => {
   const router = useRouter();
   const { handleSubmit, control } = useForm<TravelogueFormType>(travelogueFormProps);
   const { getImageUrlFromS3, deleteFile } = useImageUpload();
-
   const [travelogueId, setTravelogueId] = useState('');
   const [isEditPage, setIsEditPage] = useState(false);
   const { data: travelogue, refetch } = useGetTravelogueForEdit(travelogueId);
+  const { mutate: saveMutate } = useSaveTravelogue(isEditPage);
+  const { mutate: publishMutate } = usePatchTraveloguePublish();
 
   useEffect(() => {
     const { travelogueId, edit } = router.query;
@@ -37,21 +41,34 @@ const First = () => {
 
   const handleComplete = async (data: TravelogueFormType) => {
     const { key, url } = await getImageUrlFromS3(data.thumbnail as File);
-    console.log('isEditPage', isEditPage);
-    const response = isEditPage
-      ? await patchTravelogueForEdit({
-          data: { ...data, thumbnail: url },
-          travelogueId,
-        })
-      : await postTravelogue({ ...data, thumbnail: url });
-    if (response.status !== 200) {
-      deleteFile(key);
-      return;
-    }
-    goToSubTravelogue(response.data);
+    saveMutate(
+      { data: { ...data, thumbnail: url }, travelogueId },
+      {
+        onSuccess: ({ data }) => {
+          isEditPage ? publishTravelogue(data.travelogueId) : goToSubTravelogue(data);
+        },
+        onError: () => {
+          deleteFile(key);
+        },
+      },
+    );
   };
 
-  const goToSubTravelogue = (data: TravelogueResponseType) => {
+  const publishTravelogue = (travelogueId: string) => {
+    publishMutate(
+      { travelogueId },
+      {
+        onSuccess: ({ data }) => {
+          router.push({
+            pathname: '/detail',
+            query: { travelogueId: data.travelogueId },
+          });
+        },
+      },
+    );
+  };
+
+  const goToSubTravelogue = (data: TravelogueSaveResponseType) => {
     const { id: travelogueId, days } = data;
     setItem(`travelogueInfo`, {
       id: travelogueId,
@@ -62,12 +79,19 @@ const First = () => {
       '/post/detail',
     );
   };
+
   return (
     <form onSubmit={handleSubmit(handleComplete)}>
       <PostBasic control={control} isEditPage={isEditPage} data={travelogue?.data} />
-      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <Button type='submit'>다음</Button>
-      </Box>
+      <Stack direction='row' justifyContent='flex-end' sx={{ mt: 3, mb: 6 }}>
+        {isEditPage ? (
+          <Button type='submit' fullWidth variant='contained'>
+            완료
+          </Button>
+        ) : (
+          <Button type='submit'>다음</Button>
+        )}
+      </Stack>
     </form>
   );
 };
