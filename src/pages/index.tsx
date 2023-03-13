7ca83b5fc560c49f1af9470bb40dc2190fcc79d7
kmww <kmw9708@gmail.com>
2023-03-14 01:33:48 +0900

    chore: 파비콘 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           name='viewport'
           content='width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1'
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon.png' />
       </Head>
       <main>
         <PopularFeedList />
