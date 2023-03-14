fe8715aad7e490a978cf3a694b10cd0dc41ac22a
hyejun <solar3070@naver.com>
2023-03-14 16:40:14 +0900

    design: 게시글 관련 이미지 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -1,7 +1,9 @@
+export { default as addPost } from './addPost.svg';
 export { default as avatar } from './avatar.svg';
 export { default as bigLogo } from './bigLogo.svg';
 export { default as folder } from './folder.svg';
 export { default as location } from './location.svg';
 export { default as logo } from './logo.svg';
 export { default as scrapBg } from './scrapBg.svg';
+export { default as tempPost } from './tempPost.svg';
 export { default as worldMap } from './worldMap.svg';
