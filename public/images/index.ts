1b1c969602d0b4451c82c6a4c47999fb08d02900
hyejun <solar3070@naver.com>
2023-03-14 17:14:41 +0900

    design: 하늘색 로고 추가

--- public/images/index.ts
+++ public/images/index.ts
@@ -4,6 +4,7 @@ export { default as bigLogo } from './bigLogo.svg';
 export { default as folder } from './folder.svg';
 export { default as location } from './location.svg';
 export { default as logo } from './logo.svg';
+export { default as logoBlue } from './logoBlue.svg';
 export { default as scrapBg } from './scrapBg.svg';
 export { default as tempPost } from './tempPost.svg';
 export { default as worldMap } from './worldMap.svg';
